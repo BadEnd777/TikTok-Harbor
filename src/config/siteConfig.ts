fa052ecf1ddd352cbea09129daef3a5df503455e
BadEnd777 <badend23@hotmail.com>
2024-02-02 12:10:48 +0700

    refactor: update siteConfig and head content in +layout.svelte

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -1,9 +1,9 @@
 export const siteConfig = {
 	title: 'TikTok Harbor',
 	description:
-		'TikTok Harbor is a free online tool to download TikTok videos / slideshows from TikTok without watermark.',
-	url: 'https://tiktok-harbor.pages.dev',
-	image: '/images/opengraph.png',
+		'TikTok Harbor is a free online tool that allows you to download TikTok videos without the watermark. You can also download TikTok slideshows in HD quality.',
+	url: 'https://tiktok-harbor.pages.dev/',
+	ogImage: 'https://tiktok-harbor.pages.dev/images/opengraph.png',
 	repository: 'https://github.com/Onyx-Innovators/TikTok-Harbor',
 	keywords: [
 		'TikTok',
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,26 +1,26 @@
 <script>
 	import { ModeWatcher } from 'mode-watcher';
 	import { siteConfig } from '@/config/siteConfig';
-	import '$src/app.pcss';
 
-	const openGraph = `${siteConfig.url}${siteConfig.image}`;
+	import '$src/app.pcss';
 </script>
 
 <svelte:head>
-	<meta name="msapplication-TileColor" content="#000000" />
-	<meta name="theme-color" content="#000000" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<meta name="keywords" content={siteConfig.keywords.join(', ')} />
+	<title>{siteConfig.title} - TikTok Downloader</title>
 	<meta name="description" content={siteConfig.description} />
-	<meta name="og:description" content={siteConfig.description} />
+
+	<meta property="og:url" content={siteConfig.url} />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content={`${siteConfig.title} - TikTok Downloader`} />
+	<meta property="og:description" content={siteConfig.description} />
+	<meta property="og:image" content={siteConfig.ogImage} />
+
 	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:image" content={openGraph} />
-	<meta name="twitter:site:domain" content={siteConfig.url} />
-	<meta name="twitter:url" content={siteConfig.url} />
-	<meta name="og:title" content={siteConfig.title} />
-	<meta name="og:image" content={openGraph} />
-	<meta name="apple-mobile-web-app-title" content="Chat Bridge" />
-	<link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
+	<meta property="twitter:domain" content={siteConfig.url} />
+	<meta property="twitter:url" content={siteConfig.url} />
+	<meta name="twitter:title" content={`${siteConfig.title} - TikTok Downloader`} />
+	<meta name="twitter:description" content={siteConfig.description} />
+	<meta name="twitter:image" content={siteConfig.ogImage} />
 </svelte:head>
 
 <ModeWatcher defaultMode={'dark'} />
