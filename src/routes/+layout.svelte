051f33fbfed34fa00352b28bc8b23d6fd9b03a7b
BadEnd777 <badend23@hotmail.com>
2024-02-02 12:22:52 +0700

    style: improve HTML structure and meta tags in app.html and layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,7 @@
 <script>
 	import { ModeWatcher } from 'mode-watcher';
-	import { siteConfig } from '@/config/siteConfig';
-
 	import '$src/app.pcss';
 </script>
-
-<svelte:head>
-	<title>{siteConfig.title} - TikTok Downloader</title>
-	<meta name="description" content={siteConfig.description} />
-
-	<meta property="og:url" content={siteConfig.url} />
-	<meta property="og:type" content="website" />
-	<meta property="og:title" content={`${siteConfig.title} - TikTok Downloader`} />
-	<meta property="og:description" content={siteConfig.description} />
-	<meta property="og:image" content={siteConfig.ogImage} />
-
-	<meta name="twitter:card" content="summary_large_image" />
-	<meta property="twitter:domain" content={siteConfig.url} />
-	<meta property="twitter:url" content={siteConfig.url} />
-	<meta name="twitter:title" content={`${siteConfig.title} - TikTok Downloader`} />
-	<meta name="twitter:description" content={siteConfig.description} />
-	<meta name="twitter:image" content={siteConfig.ogImage} />
-</svelte:head>
 
 <ModeWatcher defaultMode={'dark'} />
 <slot />
